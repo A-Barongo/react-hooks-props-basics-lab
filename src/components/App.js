78ce7a438857832a12c15d235f33746e77a66e1f
Allan <allanbarongo5@gmail.com>
2025-04-09 21:39:24 +0300

    Completed react-hooks props basics lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import NavBar from "./NavBar.js";
+import Home from "./Home.js";
+import About from "./About.js";
+import Links from "./Links.js";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
-
+const { name, city,color, bio, links } = user;
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={name} city={city} color={color}/>
+      <About bio={bio} />
+      <Links github={links.github} linkedin={links.linkedin}/>
     </div>
   );
 }
